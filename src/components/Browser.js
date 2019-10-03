cb30146cf66bfcb5bbd67486c0b49567018c7292
Richard Ryan Garcia <garcia.richard.b@heb.com>
2019-10-02 20:28:57 -0500

    added basic form and access to contract through drizzle context

--- src/components/Browser.js
+++ src/components/Browser.js
@@ -5,6 +5,14 @@ import KittyCoreABI from "../contracts/KittyCoreABI.json";
 import { CONTRACT_NAME, CONTRACT_ADDRESS } from "../config";
 
 class Browser extends Component {
+  constructor(props, context) {
+    super(props);
+    this.state = { kittyId: 0 };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.contracts = context.drizzle.contracts;
+  }
+
   componentDidMount() {
     const web3 = new Web3(window.web3.currentProvider);
     // Initialize the contract instance
@@ -19,12 +27,31 @@ class Browser extends Component {
     });
   }
 
+  handleChange(event) {
+    this.setState({ kittyId: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert("kitty search for " + this.state.kittyId);
+    event.preventDefault();
+  }
+
   render() {
+    console.log(this.contracts.CryptoKitties);
+
     return (
       <div className="browser">
         <h1>Kitty Browser</h1>
 
         {/* Input to type in the kitty ID here */}
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={this.state.kittyId}
+            onChange={this.handleChange}
+          />
+          <button type="submit">Submit</button>
+        </form>
 
         {/* Display Kitty info here */}
       </div>
