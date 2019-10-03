398fe41e8f56d6defc6d2878488820adf07b8484
Richard Ryan Garcia <garcia.richard.b@heb.com>
2019-10-02 19:40:01 -0500

    used etherscan to get contract abi for KittyCore contract. contract is now being added to drizzle store and ready to be called

--- src/components/Browser.js
+++ src/components/Browser.js
@@ -1,35 +1,28 @@
-import React, { Component } from 'react';
-import { object } from 'prop-types';
-// import Web3 from 'web3';
-// import KittyCoreABI from 'contracts/KittyCoreABI.json';
-// import { CONTRACT_NAME, CONTRACT_ADDRESS } from '../config';
+import React, { Component } from "react";
+import { object } from "prop-types";
+import Web3 from "web3";
+import KittyCoreABI from "../contracts/KittyCoreABI.json";
+import { CONTRACT_NAME, CONTRACT_ADDRESS } from "../config";
 
 class Browser extends Component {
-  
   componentDidMount() {
-    // const web3 = new Web3(window.web3.currentProvider);
-
+    const web3 = new Web3(window.web3.currentProvider);
     // Initialize the contract instance
-
-    // const kittyContract = new web3.eth.Contract(
-    //   KittyCoreABI, // import the contracts's ABI and use it here
-    //   CONTRACT_ADDRESS,
-    // );
-
+    const kittyContract = new web3.eth.Contract(
+      KittyCoreABI, // import the contracts's ABI and use it here
+      CONTRACT_ADDRESS
+    );
     // Add the contract to the drizzle store
-
-    // this.context.drizzle.addContract({
-    //   contractName: CONTRACT_NAME,
-    //   web3Contract: kittyContract,
-    // });
+    this.context.drizzle.addContract({
+      contractName: CONTRACT_NAME,
+      web3Contract: kittyContract
+    });
   }
 
   render() {
     return (
       <div className="browser">
-        <h1>
-          Kitty Browser
-        </h1>
+        <h1>Kitty Browser</h1>
 
         {/* Input to type in the kitty ID here */}
 
@@ -40,7 +33,7 @@ class Browser extends Component {
 }
 
 Browser.contextTypes = {
-  drizzle: object,
+  drizzle: object
 };
 
 export default Browser;
